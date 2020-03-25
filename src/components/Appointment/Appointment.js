ac560e169563686c72f0aba0f2fb3e56d733d5f7
Pulse6 <marco698698@hotmail.com>
2020-03-25 19:27:23 -0400

    added netlify.toml

--- src/components/Appointment/Appointment.js
+++ src/components/Appointment/Appointment.js
@@ -1,3 +1,9 @@
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 import React, {useEffect} from "react";
 
 import useVisualMode from "../../hooks/useVisualMode";
